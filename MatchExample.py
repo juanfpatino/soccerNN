4ad4f86f2284ef7e975a739debbbabb68efe264d
Juan Patino <juan9patino9@gmail.com>
2022-05-12 10:59:25 -0400

    Initial commit

---
+++ MatchExample.py
@@ -0,0 +1,27 @@
+class MatchExample:
+    def __init__(self, features, label, name):
+        self.features: list = features  # list of attributes (floats)
+        self.result: int = label  # 0 if home win
+        # 1 if away win
+        # 2 if draw
+        # 3 if no bet (upset)
+        self.name: str = name
+
+    def getFeatures(self):
+        return self.features
+
+    def getLabel(self):
+        return self.result
+
+    def __str__(self):
+
+        if self.result == 0:
+            res = "Home win"
+        elif self.result == 1:
+            res = "Draw"
+        elif self.result == 2:
+            res = "Away win"
+        else:
+            res = "No bet"
+
+        return self.name + " | Label: " + res
---
+++ main.py
@@ -0,0 +1,111 @@
+import tensorflow as tf
+import csv
+from keras.models import Sequential
+from keras.layers import Dense
+from MatchExample import MatchExample
+
+"""
+Juan Francisco Patino
+
+Professional soccer game result neural network
+
+features: #TODO
+
+labels: home win, draw, away win, no bet
+
+//label as "no bet" if obvious upset
+
+"""
+
+# control
+
+matchExamples = []  # array of match example objects
+label_names = []
+
+features = 0  # determined after reading in CSV
+number_of_examples = 0  # determined after reading in CSV
+max_second_dim = 0
+input_shape = (number_of_examples, 1)
+
+
+def fromCSV():
+    global number_of_examples
+    global features
+    global matchExamples
+    global max_second_dim
+
+    with open('MatchExamples - Soccer.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        x = 0
+        for row in reader:
+            if x < 2:  # ignore first two rows
+                if x == 1:
+                    y = 0
+                    for col in row:
+                        if y < 5:
+                            y = y + 1
+                            continue
+                        label_names.append(col)
+                x = x + 1
+                continue
+            else:
+                features = row.__len__() - 5
+                number_of_examples = number_of_examples + 1
+                this_match_attributes = []
+                y = 0
+                for col in row:
+                    if y < 5:
+                        y = y + 1
+                        continue
+                    else:
+
+                        val = int(col).__abs__()
+                        this_match_attributes.append(val)
+                        if val > max_second_dim:
+                            max_second_dim = val
+
+                if row[0] == '1':
+                    label = 0
+                elif row[1] == '1':
+                    label = 1
+                elif row[2] == '1':
+                    label = 2
+                else:
+                    label = 3
+
+                this_match = MatchExample(this_match_attributes, label, row[4])
+                matchExamples.append(this_match)
+
+
+def train_then_predict():
+    global features
+    global input_shape
+
+    # todo: input layer
+    model = Sequential([
+        Dense(features * 3 / 2, input_shape=input_shape, acivation='relu'),
+        Dense(features * 3 / 2, activation='relu'),
+        Dense(4, activation='sigmoid'),
+    ])
+    print(model.summary())
+
+    inputList = []
+    output = []
+
+    for m in matchExamples:
+        inputList.append(m.getFeatures())
+        output.append(m.getLabel())
+
+    model.fit(inputList, output, 1, 10)
+
+    predict_att = []
+
+    for s in label_names:
+        p = input("Insert value (0 - " + max_second_dim.__str__() + ") For: " + s)
+        predict_att.append(p)
+
+
+if __name__ == '__main__':
+    fromCSV()
+    train_then_predict()
+
